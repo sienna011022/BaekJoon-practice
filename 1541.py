f88a4b14637ef656e5ab9e99c0aebb20b2c3e5e2
sienna <20200803@sungshin.ac.kr>
2022-03-07 21:22:28 +0900

    1541번 주유소

--- 1541.py
+++ 1541.py
@@ -1,11 +1,30 @@
-arr = input().split('-')
-s = 0
-for i in arr[0].split("+"):
-    s += int(i)
-for j in arr[1:]:
-    for t in j.split("+"):
-        s -= int(t)
-print(s)
+N = int(input())
+line = list(map(int, input().split()))
+price = list(map(int, input().split()))
+line_sum = 0
+sum = 0
+max = 0
+
+for i in range(len(line)):
+    line_sum += line[i]
+
+sort_price = list()
+
+for i in range(N-1):
+    sort_price.append(price[i])
+    sort_price.sort()
+
+max = sort_price[0]
+max = price.index(max)
+
+for i in range(max):
+    sum += price[i] * line[i]
+    line_sum -= line[i]
+
+sum += price[max] * line_sum
+
+print(sum)
+
 
 
 
