3dd820304e49adb19fde9fa56398d16b4d9dce73
sienna <20200803@sungshin.ac.kr>
2022-03-09 11:33:14 +0900

    Update 1260.py
    https://sienna1022.tistory.com/entry/%EB%B0%B1%EC%A4%80-%ED%8C%8C%EC%9D%B4%EC%8D%AC-1260%EB%B2%88-DFS%EC%99%80-BFS

--- 1260.py
+++ 1260.py
@@ -1,20 +1,42 @@
-n,m,v = map(int,input().split())
-#리스트 인덱스 0을 없애기 위함.
-graph = [[] for _ in range (1)]
 
-for i in range(m):
-    first,end = map(int,input().split())
-    graph.append([first,end])
-print(graph)
-visited = [False] * m
-def dfs(graph,v,visited):
-    visited[v] = True
-    print(v,end=' ')
-    #그 안을 탐색 [1,2]면 1을 탐색
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph,i,visited)
+from collections import deque
+N,M,V = map(int,input().split())
 
-dfs(graph,1,visited)
+#이차원 행렬을 정의한다.
+matrix = [[0] * (N+1) for i in range(N+1)]
 
+#연결된 노드들을 1로 표시한다
+for i in range(M):
+    a,b = map(int,input().split())
+    matrix[a][b] = matrix[b][a]=1
 
+#확인용 visit_list정의
+visit_list1 = [0] * (N + 1)
+visit_list2 = [0] * (N + 1)
+
+def dfs(V):
+    visit_list1[V] = 1
+    print(V,end=' ')
+    for i in range(1,N+1):
+        #방문기록이 없고 V와 인접하는 경우 재귀
+        if(visit_list1[i] == 0 and matrix[V][i] == 1):
+            dfs(i)
+
+def bfs(V):
+    #queue에 V를 넣어 첫번쨰 원소는 미리 넣어두기
+    queue = deque([V])
+    #첫번째꺼는 수동으로 설정
+    visit_list2[V] = 1
+    #이 아래는 while문이라서 맨 마지막에 visit_list[i]=1을 해줘야함.
+    while queue:
+        #queue에 들어있는 첫번쨰 원소를 뽑는다 없애는 상태.
+        V = queue.popleft()
+        print(V,end= ' ')
+        #여기서 V와 인접한 모든 원소를 탐색하게됨
+        for i in range(1,N+1):
+            if(visit_list2[i] == 0 and matrix[V][i] == 1):
+                queue.append(i)
+                visit_list2[i] = 1
+dfs(V)
+print(' ')
+bfs(V)
