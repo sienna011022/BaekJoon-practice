2a3d6ee328b88f89096dfb3d396bf34b188f5c01
sienna <20200803@sungshin.ac.kr>
2022-03-28 10:42:41 +0900

    2178 미로탐색

--- 2178.py
+++ 2178.py
@@ -1,40 +1,41 @@
 from collections import deque
 
 N,M = map(int,input().split())
+visited = [[0]*M for _ in range(N)]
+
 graph = []
 for i in range(N):
     graph.append(list(map(int,input())))
-print(graph)
+
 
 
 
 
 def bfs(x,y):
-
-    graph[0][0] = 0
     queue = deque()
     queue.append((x,y))
+    visited[0][0] = 1
+
 
     while queue:
-        x,y = queue.popleft()
-        dy = [-1,1,0,0]
-        dx = [0,0,-1,1]
+        x, y = queue.popleft()
 
+        if x == N - 1 and ny == M - 1:
+            print(visited[x][y])
+            break
         for i in range(4):
+            dx = [-1, 1, 0, 0]
+            dy = [0, 0, -1, 1]
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
-                if nx == N-1 and ny == M-1:
-                    return
-
-            #이동코드
-                graph[nx][ny] = graph[x][y] + 1
-                print(graph[nx][ny])
-                queue.append((nx,ny))
 
 
 
+            if 0 <= nx < N and 0 <= ny < M:
+                if visited[nx][ny] == 0 and graph[nx][ny] == 1:
+                    visited[nx][ny] = visited[x][y] + 1
+                    queue.append((nx, ny))
 
 
 
@@ -44,4 +45,17 @@ def bfs(x,y):
 
 
 bfs(0,0)
-print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
